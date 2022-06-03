95a871abb3f455fe7ec32f65db38711161ab189d
David Castro <david.dev.designer@gmail.com>
2022-06-03 07:52:04 -0300

    Organizando e iniciando camada de view

--- app/src/main/java/br/com/davidcastro/githubrepositories/presenter/view/activity/MainActivity.kt
+++ app/src/main/java/br/com/davidcastro/githubrepositories/presenter/view/activity/MainActivity.kt
@@ -1,13 +1,17 @@
-package br.com.davidcastro.githubrepositories
+package br.com.davidcastro.githubrepositories.presenter.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.davidcastro.githubrepositories.ui.main.MainFragment
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import br.com.davidcastro.githubrepositories.R
+import br.com.davidcastro.githubrepositories.presenter.view.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
